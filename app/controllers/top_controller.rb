6389f7215878c3a1e2e4b39a47033870ed652c9a
Hiroto Fujita <2311140069@ed.fuk.kindai.ac.jp>
2025-10-14 11:05:21 +0000

    kiso-2

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -8,11 +8,17 @@ class TopController < ApplicationController
     end
         
     def login
-        if params[:uid] == "kindai" and params[:pass] == "sanriko"
+        if User.find_by(uid:params[:uid]) and User.find_by(pass:params[:pass])
+    
             session[:login_uid] = params[:uid]
             redirect_to
         else
             render "error", status: 422
         end
+    end
+    
+    def logout
+        session.delete(:login_uid)
+        redirect_to top_main_path
     end
 end
